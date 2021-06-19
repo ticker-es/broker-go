3c4c67df5bced566907635ed02ad994cb196ed18
Max Trense <dev@trense.info>
2021-06-19 16:46:32 +0200

    Add postgres SequenceStore

--- backends/postgres/sequencestore.go
+++ backends/postgres/sequencestore.go
@@ -1,6 +1,10 @@
 package postgres
 
 import (
+	"context"
+	"errors"
+
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	es "github.com/ticker-es/client-go/eventstream/base"
 )
@@ -16,9 +20,21 @@ func NewSequenceStore(db *pgxpool.Pool) es.SequenceStore {
 }
 
 func (s *SequenceStore) Get(persistentClientID string) (int64, error) {
-	panic("implement me")
+	var sequence int64
+	ctx := context.Background()
+	err := s.db.AcquireFunc(ctx, func(c *pgxpool.Conn) error {
+		return c.QueryRow(ctx, "SELECT last_acknowledged_sequence FROM subscribers WHERE id = $1", persistentClientID).Scan(&sequence)
+	})
+	if errors.Is(err, pgx.ErrNoRows) {
+		return 0, nil
+	}
+	return sequence, err
 }
 
 func (s *SequenceStore) Store(persistentClientID string, sequence int64) error {
-	panic("implement me")
+	ctx := context.Background()
+	return s.db.AcquireFunc(ctx, func(c *pgxpool.Conn) error {
+		_, err := c.Exec(ctx, "INSERT INTO subscribers (id, last_acknowledged_sequence) VALUES ($1, $2) ON CONFLICT ON CONSTRAINT subscribers_pkey DO UPDATE SET last_acknowledged_sequence = $2", persistentClientID, sequence)
+		return err
+	})
 }
